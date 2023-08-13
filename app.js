a89e4bd963fb8934cea481eb502ba23c520dec8d
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-08-13 12:11:35 +0200

    ++added missing error icon

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let input = document.querySelector("input");
 let button = document.querySelector("button");
 let success = document.querySelector(".success");
 let error = document.querySelector(".error");
+let erroricon = document.querySelector(".erroricon");
 
 button.addEventListener("click", (e) => {
   let email = input.value; // Move the email assignment inside the event listener
@@ -9,8 +10,10 @@ button.addEventListener("click", (e) => {
   if (email.includes("@") && email.length > 11) {
     success.style.display = "block";
     error.style.display = "none";
+    erroricon.style.display = "none";
   } else {
     error.style.display = "block";
+    erroricon.style.display = "block";
     success.style.display = "none";
   }
 });
